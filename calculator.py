04d21cc02c3a094678d52d5605e7aac2bbfec2b7
devpatel2103 <devpatel2103@gmail.com>
2023-09-19 15:04:10 -0400

    Added error handlers

--- calculator.py
+++ calculator.py
@@ -28,8 +28,11 @@ def multiply(num1, num2):
 
 @my_decorator
 def divide(num1, num2):
-    ans = f'{num1} / {num2} = {num1 / num2}'
-    print(ans)
+    try:
+        ans = f'{num1} / {num2} = {num1 / num2}'
+        print(ans)
+    except ZeroDivisionError:
+        print('You cannot divide by zero! Try again')
 
 def again():
     end = input('Do you have something else to calculate? (y/n)')
@@ -42,44 +45,46 @@ def again():
         again()
 
 def calculator():
+    try:
+        selection = input('''
+            What Operation do you want?
+        
+            1. Addition
+            2. Subtraction 
+            3. Multiplication 
+            4. Division
+            ''')
+        
+        if(selection == '1'):
+            print("You have selected Addition")
+            num1 = int(input("First Number: "))
+            num2 = int(input("Second Number: "))
+            add(num1,num2)
+        
+        elif(selection == '2'):
+            print("You have selected Subtraction")
+            num1 = int(input("First Number: "))
+            num2 = int(input("Second Number: "))
+            subtract(num1,num2)
+        
+        elif(selection == '3'):
+            print("You have selected Multiplication")
+            num1 = int(input("First Number: "))
+            num2 = int(input("Second Number: "))
+            multiply(num1,num2)
 
-    selection = input('''
-        What Operation do you want?
-    
-        1. Addition
-        2. Subtraction 
-        3. Multiplication 
-        4. Division
-        ''')
-    
-    if(selection == '1'):
-        print("You have selected Addition")
-        num1 = int(input("First Number: "))
-        num2 = int(input("Second Number: "))
-        add(num1,num2)
-    
-    elif(selection == '2'):
-        print("You have selected Subtraction")
-        num1 = int(input("First Number: "))
-        num2 = int(input("Second Number: "))
-        subtract(num1,num2)
-    
-    elif(selection == '3'):
-        print("You have selected Multiplication")
-        num1 = int(input("First Number: "))
-        num2 = int(input("Second Number: "))
-        multiply(num1,num2)
-
-    elif(selection == '4'):
-        print("You have selected Division")
-        num1 = int(input("First Number: "))
-        num2 = int(input("Second Number: "))
-        divide(num1,num2)
-    
-    else:
-        print("Your Input was Invalid, Try again!")
-    
-    again()
+        elif(selection == '4'):
+            print("You have selected Division")
+            num1 = int(input("First Number: "))
+            num2 = int(input("Second Number: "))
+            divide(num1,num2)
+        
+        else:
+            print("Your Input was Invalid, Try again!")
+        
+        again()
+    except:
+        print('There was an error')
 
 welcome()
 calculator()
